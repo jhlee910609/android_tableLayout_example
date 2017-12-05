9b66451307ebd1bdc0e77d60acec9a4151ae5cbb
Junhee Lee <jhlee910609@umsun.co.kr>
2017-12-05 10:12:27 +0900

    example of scheme, host

--- app/src/main/java/com/example/umsun_android/tablelayoutexam/Main2Activity.java
+++ app/src/main/java/com/example/umsun_android/tablelayoutexam/Main2Activity.java
@@ -1,17 +1,40 @@
 package com.example.umsun_android.tablelayoutexam;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 public class Main2Activity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        checkIntent();
+    }
 
-        Intent intent = new Intent(this, Main2Activity.class);
-        startActivity(intent);
+    private void checkIntent(){
+        if (getIntent() != null && getIntent().getAction() == Intent.ACTION_VIEW){
+            Toast.makeText(this, getIntent().getData().getQueryParameter("color"), Toast.LENGTH_SHORT).show();
+            setBackground(getIntent().getData().getQueryParameter("color"));
+        }
+    }
+
+    private void setBackground(String colorName){
+        switch (colorName){
+            case "blue":
+                findViewById(R.id.main2_bg).setBackgroundColor(Color.BLUE);
+                break;
+
+            case "red":
+                findViewById(R.id.main2_bg).setBackgroundColor(Color.RED);
+                break;
+        }
+
+
     }
 }
--- app/src/main/java/com/example/umsun_android/tablelayoutexam/MainActivity.java
+++ app/src/main/java/com/example/umsun_android/tablelayoutexam/MainActivity.java
@@ -1,15 +1,38 @@
 package com.example.umsun_android.tablelayoutexam;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    LinearLayout layout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        findViewById(R.id.btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = "umsun://com.app.umsun.main_3";
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                startActivity(intent);
 
+            }
+        });
+
+        findViewById(R.id.btn2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = "umsun://com.app.umsun.homeactivity?color=red";
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                startActivity(intent);
+            }
+        });
     }
 }
